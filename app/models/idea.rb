54655a865c5def4074d9f2868885d1a511b5a6e3
LJruby <labuda.jakub@gmail.com>
2015-02-04 23:04:29 +0100

    v2

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,4 @@
 class Idea < ActiveRecord::Base
+  validates_presence_of :title
+  belongs_to :status
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203203016) do
+ActiveRecord::Schema.define(version: 20150204193652) do
 
   create_table "ideas", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20150203203016) do
     t.integer  "customer_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
