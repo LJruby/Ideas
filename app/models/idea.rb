ad44a4f33b3c5963c3c3e31c5cf4a289a6202760
LJruby <labuda.jakub@gmail.com>
2015-02-08 17:39:25 +0100

    v6

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,7 +2,9 @@ class Idea < ActiveRecord::Base
   validates :title,
     presence: true,
     uniqueness: true
-    
+  validates :status_id, :category_id, :customer_id,
+    presence: true
+
   belongs_to :status
   belongs_to :customer
   belongs_to :category
